1bfb99d88cfc746855f0c007bd9d5b0ac432f3eb
PorPla <apiwat.sri@icloud.com>
2021-12-12 10:26:47 +0000

    first commit

---
+++ vendor/vimeo/psalm/src/Psalm/Type/Atomic/TCallableArray.php
@@ -0,0 +1,13 @@
+<?php
+namespace Psalm\Type\Atomic;
+
+/**
+ * Denotes an array that is _also_ `callable`.
+ */
+class TCallableArray extends TNonEmptyArray
+{
+    /**
+     * @var string
+     */
+    public $value = 'callable-array';
+}
